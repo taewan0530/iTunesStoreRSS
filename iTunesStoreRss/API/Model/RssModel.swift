59fc2030e2590d7c52584a562ea1e1552c45654d
taewan <taewan0530@daum.net>
2017-04-20 23:41:37 +0900

    router, model 추

---
+++ iTunesStoreRss/API/Model/RssModel.swift
@@ -0,0 +1,29 @@
+//
+//  RssModel.swift
+//  iTunesStoreRss
+//
+//  Created by taewan on 2017. 4. 20..
+//  Copyright © 2017년 taewan. All rights reserved.
+//
+
+import Foundation
+
+import SwiftyJSON
+
+
+struct RssModel: JSONCollection {
+    let id: String
+    let title: String
+    let imageURL: URL?
+    
+    init(_ json: JSON) {
+        self.id = json["id"]["attributes"]["im:id"].stringValue
+        self.title = json["title"]["label"].stringValue
+        
+        if let lastImage = json["im:image"].arrayValue.last {     
+            self.imageURL = URL(string: lastImage["label"].stringValue)
+        } else {
+            self.imageURL = nil
+        }
+    }
+}
