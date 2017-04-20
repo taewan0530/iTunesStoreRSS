523fa2b20112a1cba141b92def6eb1a6d696891a
taewan <taewan0530@daum.net>
2017-04-20 20:27:34 +0900

    프로젝트 생성

---
+++ iTunesStoreRss/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iTunesStoreRss
+//
+//  Created by taewan on 2017. 4. 20..
+//  Copyright © 2017년 taewan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
